353d963a4ab5eee3ad467fb7f355006720608e54
Enzo Rosa <enzo.rosa4@etec.sp.gov.br>
2025-02-20 08:28:20 -0300

    Aula sobre tipos de variáveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,14 @@
+var numero = 2;
+let texto = "Hello";
+const oQueEisso = "?";
+
+console.log('Tipo var:', numero);
+
+numero = 13;
+
+console.log(texto + " O numero é " + numero);
+console.log(`${texto} o número agora é ${numero}`)
+
+texto = 69; 
+
+console.log(texto + numero);
