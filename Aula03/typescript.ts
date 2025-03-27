10b13a169ab80ccafc15a7e28161d9079d66d103
Enzo Rosa <enzo.rosa4@etec.sp.gov.br>
2025-03-27 08:29:26 -0300

    AulaPAMI

---
+++ Aula03/typescript.ts
@@ -0,0 +1,10 @@
+function Somar(a, b) {
+    return a + b;
+}
+console.log(Somar(5, "10"));
+
+    function somarComTipo(a: number, b: number): number {
+        return a + b;
+    }
+console.log(somarComTipo(5, 10));
+
